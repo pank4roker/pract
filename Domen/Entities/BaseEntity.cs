31845fedaaa4f0d664347a148e1821c90d87a4aa
Markow Kirill <kirillmarkor@gmail.com>
2024-03-20 17:25:52 +0200

    1 lesson

---
+++ Domen/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domen.Entities
+{
+    internal class BaseEntity
+    {
+    }
+}
